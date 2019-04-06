db79ac0fe9eca3e3bf6e18c9a965c78a45aa1f87
huangjj27 <huangjj.27@qq.com>
2019-04-06 11:35:04 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! This lib contains problems and contests' solutions by [huangjj27].
+//! I will try my best to provide example, and my test cases.
+//! 
+//! [huangjj27]: https://github.com/huangjj27
+
+pub mod problems {
+
+}
+
+pub mod contests {
+    pub mod contest130;
+}
