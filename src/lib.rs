ff40a165faaf49375d430b5c0283290a2932f2d7
neetdai <ake17101@gmail.com>
2019-04-09 18:02:31 +0800

    将problem7添加到路径上

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,18 @@
 #![allow(non_snake_case)]
 
 pub mod problems {
+    pub mod problem1;
+    pub mod problem100;
     pub mod problem121;
     pub mod problem122;
+    pub mod problem14;
+    pub mod problem2;
+    pub mod problem20;
+    pub mod problem58;
+    pub mod problem66;
+    pub mod problem67;
+    pub mod problem69;
+    pub mod problem7;
 }
 
 pub mod contests {
